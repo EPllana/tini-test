1a6269daaac181546d4032ecd12fdc39b62dab0e
Ernis Pllana <epllana03@gmail.com>
2025-04-11 01:00:19 +0200

    puff baboo

--- scrip.js
+++ scrip.js
@@ -7,21 +7,7 @@ menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-  const swiper = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+ 
+
+
 
